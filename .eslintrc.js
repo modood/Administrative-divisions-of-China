7db4fb46edf30560ca45565042d55871c76ed7a0
modood <modood@qq.com>
2016-12-20 16:34:17 +0800

    使用 Eslint 检查符号 standard 规范的代码

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: 'standard',
+  globals: {
+    'describe': true,
+    'context': true,
+    'before': true,
+    'beforeEach': true,
+    'after': true,
+    'afterEach': true,
+    'it': true,
+    'expect': true
+  }
+}
+
